fd7ef505399bcd7e52efdd0dc6e63c45a8c73caa
Manuel Bugallo <marvinody@gmail.com>
2022-08-26 13:47:55 -0400

    this is what I get for not testing

--- src/bot/events/messageDeleteBulk.js
+++ src/bot/events/messageDeleteBulk.js
@@ -37,12 +37,11 @@ async function paste (messages, guildID) {
   if (pasteString) {
     sa
       .post(process.env.PASTE_CREATE_ENDPOINT)
-      .set('Content-Type', 'text/plain')
       .send({
         content: pasteString || 'An error has occurred while fetching pastes. Please contact the bot author.'
       })
       .end((err, res) => {
-        if (!err && res.body && res.statusCode === 200 && res.body.result.key) {
+        if (!err && res.body && res.statusCode === 201 && res.body.result.key) {
           messageDeleteBulkEvent.embeds[0].fields.push({
             name: 'Link',
             value: `https://nekobin.com/${res.body.result.key}`
